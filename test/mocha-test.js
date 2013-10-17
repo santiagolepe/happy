cde0fb9584c9f6dad55f3770b62b179479e13051
Octavio Santiago <santiagolepe@gmail.com>
2013-10-17 16:26:08 -0500

    add the mocha test

---
+++ test/mocha-test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+  describe('#indexOf()', function(){
+    it('Should return -1 when the value isnt present', function(){
+      assert.equal(-1, [1,2,3].indexOf(6));
+    });
+  });
+
+
