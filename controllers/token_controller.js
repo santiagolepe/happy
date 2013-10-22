92f1366985a448431dab912087ff3cce5e92d7d5
Octavio Santiago <santiagolepe@gmail.com>
2013-10-22 13:40:54 -0500

    add the token controller for get token and renew

--- controllers/token_controller.js
+++ controllers/token_controller.js
@@ -3,12 +3,29 @@ var User = require('../models/user.js');
 module.exports = {
 
   getToken: function(req){
-    var email = req.params.email;
-
+    var email = req.auth.credentials.email;
     User.findOne({email: email}, function(err, usr){
       if(err){ return req.reply(err); }
 
       req.reply(usr.token).code(200);
+    });
+  },
+
+  renew: function(req){
+    var email = req.pre.user.email;
+    
+    User.findOne({email: email}, function(err, usr){
+      if(err){ return req.reply(err); }
+      
+      //generate another token
+      usr.createToken(function(){
+
+        usr.save(function(err, usr2){
+          if(err){ return req.reply(err); }
+
+          req.reply(usr2.token);
+        });
+      });
     });
   }
 
