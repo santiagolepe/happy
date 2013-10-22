bb61f3250fc11eafa924df14c0e0a4f1d520dd56
Octavio Santiago <santiagolepe@gmail.com>
2013-10-22 13:42:45 -0500

    add the user model, with 2 methods(encrypPassword and generate token)

--- models/user.js
+++ models/user.js
@@ -1,21 +1,49 @@
-module.exports = function(Mongoose){
-  
-  var Schema = Mongoose.Schema;
+var mongoose = require('mongoose'),
+    validate = require('mongoose-validate'),
+      bcrypt = require("bcrypt"),
+      Schema = mongoose.Schema;
 
-  Mongoose.model('user', new Schema({
+var userSchema = new Schema({
     
-    first_name: {type: 'String', required: true},
-    last_name:  {type: 'String', required: true},
-    email:      {type: 'String', required: true},
-    token:      {type: 'String', required: true},
-    harvest_email:{type: 'String', required: true},
-    harvest_pwd:{type: 'String', required: true},
-    pivotal:    {type: 'String', required: true},
-    password:   {type: 'String', required: true},
-    created_at: {type: Date, default: Date.now}
+    first_name:   {type: 'String', required: true},
+    last_name:    {type: 'String', required: true},
+    email:        {type: 'String', required: true, unique: true, index: true, lowercase:true, validate: [validate.email, 'invalid email address']},
+    token:        {type: 'String'},
+    harvest_email:{type: 'String', required: true, validate: [validate.email, 'invalid email address']},
+    harvest_pwd:  {type: 'String', required: true},
+    pivotal_token:{type: 'String', required: true},
+    password:     {type: 'String', required: true, match:/^.{8,200}$/},
+    type:         {type: 'String', default: "free"},
+    created_at:   {type: Date, default: Date.now}
 
-  },{collection: 'users'}));
+});
 
-  return Mongoose.model('user');
+//method to encrypt the password
+userSchema.methods.encryptPass = function(next){
+  var self = this;
 
+  //Encrypt pass salt 10
+  bcrypt.genSalt(10, function(err, salt){
+    if(err){ return next(err); }
+    bcrypt.hash(self.password, salt, function(err, hash){
+      if(err){ return next(err) }
+      self.password = hash;
+      next();
+    });
+  });
 };
+
+//method to generate token 
+userSchema.methods.createToken = function(next){
+  var self = this;
+
+  //Generate token
+  require('crypto').randomBytes(48, function(ex, buf) {
+    self.token = buf.toString('hex');
+    next();
+  });
+};
+
+module.exports = mongoose.model('user', userSchema); 
+
+
