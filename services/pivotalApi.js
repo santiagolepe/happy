b9bac669e642e2a6703ce90531b4c048695f71e8
Octavio Santiago <santiagolepe@gmail.com>
2013-10-23 10:54:58 -0500

    improve the pivotal api, add the method put and post

--- services/pivotalApi.js
+++ services/pivotalApi.js
@@ -1,25 +1,32 @@
 var https = require('https');
-
-var opts = {
-  hostname: 'www.pivotaltracker.com',
-  path: '/',
-  method: 'GET',
-  port: 443,
-  headers: { 'X-TrackerToken': 'XXX-XXX-XXX-XXX-XXX' }
-
-};
+var opts  = require('../config/settings').pivotal;
 
 module.exports = {
 
-  projects: function(options, cb){
+  api: function(options, cb){
     opts.path                      = options.path;
     opts.headers['X-TrackerToken'] = options.token;
-  
+    opts.method                    = options.method;
+
+    //if send PUT or POST - send a body
+    if(options.body){
+      var body = JSON.stringify(options.body);
+      opts.headers['Content-Length'] = body.length;
+    }
+     
     var req = https.request(opts, function(res){
+
       res.on('data', function(data){
         cb(null, data.toString());
       });
     });
+
+    //Send body if post or put
+    if(options.body){
+      req.write(body);
+      //remove the Content-Length
+      delete opts.headers['Content-Length'];
+    }
 
     //end request
     req.end();
